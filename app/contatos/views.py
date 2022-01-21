90b2d61c2d4ca998924f4e7f32295f1bb2504859
silviotmalmeida <silviotmalmeida@gmail.com>
2022-01-21 17:58:54 -0300

    Implementacao ate aula 5

--- app/contatos/views.py
+++ app/contatos/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from .models import Contato  # importando a model Contato para exibição nas views
 
 
 # definindo a view index
 def index(request):
+
+    # retornando os registros do banco de dados
+    contatos = Contato.objects.all()
+
     # o html deve estar na pasta templates/contatos/
-    return render(request, 'contatos/index.html')
+    return render(request, 'contatos/index.html', {
+        'contatos': contatos,
+    })
