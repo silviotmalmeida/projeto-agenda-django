e51e61c116750c0659e2faf2c6a0167489e744aa
silviotmalmeida <silviotmalmeida@gmail.com>
2022-01-21 14:05:17 -0300

    Implementacao ate aula 4

--- app/contatos/admin.py
+++ app/contatos/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Categoria, Contato  # importando as models do app
 
-# Register your models here.
+
+# definindo as configurações de exibição da Categoria na área administrativa
+class CategoriaAdmin(admin.ModelAdmin):
+
+    # definindo as colunas a serem exibidas
+    list_display = ('id', 'nome')
+
+    # definindo em quais colunas serão colocados links de edição
+    list_display_links = ('nome',)
+
+    # definindo o limite de registros por página
+    list_per_page = 10
+
+    # definindo as colunas a serem consideradas no campo de pesquisa
+    search_fields = ('nome',)
+
+
+# definindo as configurações de exibição do Contato na área administrativa
+class ContatoAdmin(admin.ModelAdmin):
+
+    # definindo as colunas a serem exibidas
+    list_display = ('id', 'nome', 'sobrenome', 'telefone',
+                    'email', 'data_criacao', 'categoria')
+
+    # definindo em quais colunas serão colocados links de edição
+    list_display_links = ('nome',)
+
+    # definindo em quais colunas serão criados filtros
+    list_filter = ('categoria',)
+
+    # definindo o limite de registros por página
+    list_per_page = 10
+
+    # definindo as colunas a serem consideradas no campo de pesquisa
+    search_fields = ('nome',)
+
+
+# registrando as models para exibição na área administrativa
+admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Contato, ContatoAdmin)
--- app/contatos/models.py
+++ app/contatos/models.py
@@ -2,15 +2,41 @@ from django.db import models
 from django.utils import timezone
 
 
+# criando a model de Categoria
 class Categoria(models.Model):
+
+    # criando o atributo nome como texto com tamanho máximo de 255
     nome = models.CharField(max_length=255)
 
+    # definindo qual atributo da model será exibido na área administrativa
+    def __str__(self):
+        return self.nome
 
+
+# criando a model de Contato
 class Contato(models.Model):
+
+    # criando o atributo nome como texto com tamanho máximo de 255
     nome = models.CharField(max_length=255)
+
+    # criando o atributo sobrenome como texto com tamanho máximo de 255, opcional
     sobrenome = models.CharField(max_length=255, blank=True)
+
+    # criando o atributo telefone como texto com tamanho máximo de 255
     telefone = models.CharField(max_length=255)
+
+    # criando o atributo email como texto com tamanho máximo de 255, opcional
     email = models.CharField(max_length=255, blank=True)
+
+    # criando o atributo data_criacao como datetime atual
     data_criacao = models.DateTimeField(default=timezone.now)
+
+    # criando o atributo descricao como texto, opcional
     descricao = models.TextField(blank=True)
+
+    # criando o atributo categoria com referência à model Categoria, sem deleção em castaca
     categoria = models.ForeignKey(Categoria, on_delete=models.DO_NOTHING)
+
+    # definindo qual atributo da model será exibido na área administrativa
+    def __str__(self):
+        return self.nome
